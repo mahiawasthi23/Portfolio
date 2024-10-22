72f84cc6104f55222ad4a7dc55da5841ab86a1f0
mahima <mahima@23navgurukul.org>
2024-10-22 15:11:16 +0530

    My Portfolio

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert('Form submitted!');
+    });
+});
+
+
+
+
+
